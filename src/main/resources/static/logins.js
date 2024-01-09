5661395a9a9ea43d3f51cfc362bf38ac9a1980eb
abraham reta <agetahun567@gmail.com>
2024-01-09 11:28:30 -0500

    added login page

---
+++ src/main/resources/static/logins.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    if (username == "" || password == "") {
+        alert("Username and Password must be filled out");
+        return false;
+    }
+    return true;
+}
